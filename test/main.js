9a37d5b5b92bd711d386fd04344ea76916263360
Joaquín Bernal <jobedom@gmail.com>
2015-01-06 18:34:18 +0100

    Add render management

--- test/main.js
+++ test/main.js
@@ -5,12 +5,67 @@ var Fluzo = require('../lib');
 
 describe('fluzo', function () {
 
+   var store;
+
    beforeEach(function (done) {
+      Fluzo.clearRenderRequests();
+      Fluzo.Store.removeAll();
+      store = new Fluzo.Store('test');
       return done();
    });
 
-   it('...', function () {
-      assert(Fluzo.double(103) === 206);
+   it('access stores transparently', function () {
+      assert(store.id === 'test');
+   });
+
+   it('executes callback for rendering', function () {
+      var triggered = false;
+      var unsubscribe = Fluzo.onRender(function () {
+         assert(store.state.number === 200);
+         triggered = true;
+      });
+      store.onChangeNumber = function () {
+         this.state.number = 200;
+         this.changed();
+      };
+      store.setInitialState({
+         foo: 'bar',
+         number: 100
+      });
+      assert(!triggered);
+      Fluzo.action('change.number');
+      Fluzo.renderIfRequested();
+      assert(triggered);
+      unsubscribe();
+   });
+
+   it('executes multiple callbacks for rendering', function () {
+      var triggered1 = false;
+      var triggered2 = false;
+      var unsubscribe1 = Fluzo.onRender(function () {
+         assert(store.state.number === 200);
+         triggered1 = true;
+      });
+      var unsubscribe2 = Fluzo.onRender(function () {
+         assert(store.state.number === 200);
+         triggered2 = true;
+      });
+      store.onChangeNumber = function () {
+         this.state.number = 200;
+         this.changed();
+      };
+      store.setInitialState({
+         foo: 'bar',
+         number: 100
+      });
+      assert(!triggered1);
+      assert(!triggered2);
+      Fluzo.action('change.number');
+      Fluzo.renderIfRequested();
+      assert(triggered1);
+      assert(triggered2);
+      unsubscribe1();
+      unsubscribe2();
    });
 
 });
