ba762a5b5599e54392ea662eade2173c215ac799
Joaquín Bernal <jobedom@gmail.com>
2015-01-06 19:40:13 +0100

    Minor fixes

--- test/main.js
+++ test/main.js
@@ -18,7 +18,7 @@ describe('fluzo', function () {
       assert(store.id === 'test');
    });
 
-   it('executes callback for rendering', function () {
+   it('executes callback when rendering', function () {
       var triggered = false;
       var unsubscribe = Fluzo.onRender(function () {
          assert(store.state.number === 200);
@@ -39,7 +39,7 @@ describe('fluzo', function () {
       unsubscribe();
    });
 
-   it('executes multiple callbacks for rendering', function () {
+   it('executes multiple callbacks when rendering', function () {
       var triggered1 = false;
       var triggered2 = false;
       var unsubscribe1 = Fluzo.onRender(function () {
@@ -65,6 +65,35 @@ describe('fluzo', function () {
       assert(triggered1);
       assert(triggered2);
       unsubscribe1();
+      unsubscribe2();
+   });
+
+   it('unsubscribes rendering callbacks', function () {
+      var triggered1 = false;
+      var triggered2 = false;
+      var unsubscribe1 = Fluzo.onRender(function () {
+         assert(store.state.number === 200);
+         triggered1 = true;
+      });
+      var unsubscribe2 = Fluzo.onRender(function () {
+         assert(store.state.number === 200);
+         triggered2 = true;
+      });
+      store.onChangeNumber = function () {
+         this.state.number = 200;
+         this.changed();
+      };
+      store.setInitialState({
+         foo: 'bar',
+         number: 100
+      });
+      assert(!triggered1);
+      assert(!triggered2);
+      unsubscribe1();
+      Fluzo.action('change.number');
+      Fluzo.renderIfRequested();
+      assert(!triggered1);
+      assert(triggered2);
       unsubscribe2();
    });
 
